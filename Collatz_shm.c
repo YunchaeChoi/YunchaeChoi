1ed72f9bbf32c625acb09c6a71aea6f4180bb824
YunchaeChoi <cyunchae@arcs.skku.edu>
2021-10-22 14:11:56 +0900

    shared-memory Collatz problem

--- Collatz_shm.c
+++ Collatz_shm.c
@@ -6,10 +6,12 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
 
 int main(int argc, char **argv)
 {
-	int *t;
+	char *t;
 	int n=(int)strtol(argv[1], &t, 10);
 	if(n<=0)	{printf("negative value\n");exit(1);}
 	pid_t pid;
@@ -17,41 +19,69 @@ int main(int argc, char **argv)
 	char *ptr;
 	const char *shm="Collatz";
 	const int SIZE = 4096;
-	char msg_temp[10];
+	char msg_temp[128];
+	char *blank = " ";
 	int fd=shm_open(shm,O_RDWR | O_CREAT,0666);
 	if(fd==-1)
 	{
 		printf("shared memory failed!\n");
-		exit(1);
+		return 1;
 	}
-	ftruncate(fd,SIZE); // set the size to 4,096 bytes. 
+	ftruncate(fd,SIZE); // set the size to 4096 bytes. 
 	if((pid=fork())<0)
 	{
 		printf("fork failed!\n");
-		exit(1);
+		return 1;
 	}
 	if(pid==0)
 	{
-		ptr=(char *)mmap(0,SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		ptr=mmap(0,SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		if(ptr ==MAP_FAILED)
+		{
+			printf("Map failed!\n");
+			exit(-1);
+		}
 		sprintf(msg_temp,"%d",n);
 		sprintf(ptr,"%s",msg_temp);
-		ptr+=strlen(msg_temp);
-		while(n>1)
+		
+		while(n!=1)
 		{
+			ptr+=strlen(msg_temp);
+			sprintf(ptr,"%s",blank);
+			ptr+=strlen(blank);
+			
 			if(n%2==0)
 				n/=2;
 			else
 				n=n*3+1;
 			sprintf(msg_temp,"%d",n);
 			sprintf(ptr,"%s",msg_temp);
-			ptr+=strlen(msg_temp);
+			
 		}
 	
 	}
 	if(pid>0)
 	{
 		wait(&status);
-		printf("%s \n",(char*)ptr);
+		ptr=(char *)mmap(0,SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		while(ptr!=NULL)
+		{
+			if(*ptr-'0'<0 || *ptr-'0'>9 )
+			{
+				if(*ptr== ' ')
+				{
+					printf(" ");
+					ptr+=1;
+					continue;
+				}
+				else
+					break;
+			}
+
+			printf("%c",*ptr);
+			ptr+=1;
+		}
+		printf("\n");
 		shm_unlink(shm);
 		return 0;
 	}
