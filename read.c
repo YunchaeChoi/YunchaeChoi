a8475aedf4e380bf08617ebb9726507e1d95c5b6
YunchaeChoi <cyunchae@arcs.skku.edu>
2021-11-05 23:15:37 +0900

    wzip.c 수정 read.c 추가

---
+++ read.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define SIZE 255
+
+int main(int argc, char** argv)
+{
+	FILE *fw = fopen(argv[1],"r");
+	int data;
+	char c;
+	while(1)
+	{
+		if(fread(&data,sizeof(int),1,fw) == 0 ) break;
+		printf("%d",data);
+		if(fread(&c,sizeof(char),1,fw) == 0 ) break;
+		printf("%c",c);
+	}
+	printf("\n");
+	fclose(fw);
+	return 0;
+}
--- wzip.c
+++ wzip.c
@@ -5,74 +5,114 @@
 
 #define SIZE 255
 
+typedef struct _value
+{
+	int num;
+	char alpha;
+}value;
+
 int main(int argc, char** argv)
 {
 	FILE* fp;
-	//FILE* fw; // file to write on, i.e., file.z
-	//fw = fopen("file.z","a+");
-
-	//int size;
-	
+		
 	char input[SIZE];
 	char buffer[SIZE];
-	//char path[SIZE];
-	//char *output_c; // char
-	//int *output_i; // int
-	//char output[SIZE];
-	//memset(output,0,sizeof(output));
 	char* line = NULL;
 	size_t len=0;
 	ssize_t nread;
-
-	fp=fopen(argv[argc-1],"r");
-	nread = getline(&line,&len,fp);
-	if(nread==-1)
+	value arr[SIZE];
+	int f=0; // for arr
+	for(int t=0;t<argc-1;t++)
 	{
-		printf("getline fail\n");
-		exit(1);
-	}
-	
-	strcpy(input,line);
-	free(line);
-	int cnt=0;
-	int j=0;
-	//char *c;
-	memset(buffer,0,sizeof(buffer));
-	//printf("%ld\n",strlen(input));
-	int temp;
-	while(j<strlen(input)) //  j < 13
-	{
-		if(j==strlen(input)-1)
+		fp=fopen(argv[t+1],"r");
+		nread = getline(&line,&len,fp);
+		if(nread==-1)
 		{
-			//printf("%d%c",(int)strlen(buffer),buffer[0]);
-			//temp =(int)strlen(buffer);
-			fwrite(&temp,sizeof(int),1,stdout);
-			fwrite(buffer,sizeof(char),1,stdout);
-			break;
+			printf("getline fail\n");
+			exit(1);
 		}
-		if(cnt==0)
+		fclose(fp);
+		strcpy(input,line);
+		free(line);
+		int cnt=0;
+		int j=0;
+		memset(buffer,0,sizeof(buffer));
+		int temp=0;
+		while(1)
 		{
-			buffer[cnt] = input[j];
-			cnt++; j++; temp++;
-		}
-		else
-		{
-			if(input[j]==buffer[cnt-1])
+			if(j==strlen(input)-1)
+			{
+				if(input[j]==buffer[cnt-1])
+				{
+					buffer[cnt] = input[j];
+					cnt++; j++; temp++;
+				}
+				//fwrite(&temp,sizeof(int),1,stdout);
+				//fwrite(buffer,sizeof(char),1,stdout);
+				arr[f].num = temp;
+				arr[f].alpha = buffer[0];
+				f++;
+				cnt=0; j=0; temp=0;
+				break;
+			}
+			if(cnt==0)
 			{
 				buffer[cnt] = input[j];
 				cnt++; j++; temp++;
 			}
 			else
 			{
-				cnt=0;
-				//printf("%d%c",(int)strlen(buffer),buffer[0]);
-				//temp=(int)strlen(buffer);
-				fwrite(&temp,sizeof(int),1,stdout);
-				fwrite(buffer,sizeof(char),1,stdout);
-				memset(buffer,0,sizeof(buffer));
-				temp=0;
+				if(input[j]==buffer[cnt-1])
+				{
+					buffer[cnt] = input[j];
+					cnt++; j++; temp++;
+				}
+				else
+				{
+					cnt=0;
+					//fwrite(&temp,sizeof(int),1,stdout);
+					//fwrite(buffer,sizeof(char),1,stdout);
+					arr[f].num = temp;
+					arr[f].alpha = buffer[0];
+					f++;
+					memset(buffer,0,sizeof(buffer));
+					temp=0;
+				}
 			}
+		} // end of while
+
+	}//end of for
+	int sum=0;
+	/*
+	for(int i=0;i<f;i++)
+	{
+		if( i>0 && arr[i].alpha == arr[i-1].alpha )
+		{
+			while(arr[i].alpha == arr[i-1].alpha)
+			{
+				sum += arr[i].num + arr[i-1].num;
+				i++;
+				if( i+1 == f)
+				{
+					fwrite(&sum,sizeof(int),1,stdout);
+					fwrite(&arr[i],alpha,sizeof(char(,1,stdout);
+					break;
+				}
+			}
+			fwrite( &sum, sizeof(int),1,stdout);
+			fwrite(&(arr[i].alpha) ,sizeof(char) , 1, stdout);
 		}
+		else
+		{
+			fwrite(&(arr[i].num) , sizeof(int), 1, stdout);
+			fwrite(&(arr[i].alpha) , sizeof(char), 1, stdout);
+		}
+	}
+	*/
+	for(int i=0;i<f;i++)
+	{
+		fwrite(&arr[i].num,sizeof(int),1,stdout);
+		fwrite(&arr[i].alpha,sizeof(char),1,stdout);
 	}
 	return 0;
 }
