d15da6f41d1a9775a17bdd37fafdd5dc7e4f3222
YunchaeChoi <cyunchae@arcs.skku.edu>
2021-11-15 20:07:23 +0900

    wish.c stuck at test case 7

--- ostep-project/wish.c
+++ ostep-project/wish.c
@@ -40,7 +40,6 @@ int main(int argc, char** argv)
 
 		setenv("PATH", "/bin",1);
 
-
 		char* line = NULL;
 		int status; // for wait
 		size_t linecap;
@@ -50,125 +49,133 @@ int main(int argc, char** argv)
 		{
 			if((nread=getline(&line,&linecap,fp))==-1) // EOF. means that the last line of argv[1] is not "exit"
 			{
-				return 0;
+				exit(0);
 			}
-			int fd[2];
-			pipe(fd); // make ordinary pipe
+			char* token;
+			char* arg[10];
+			assert(line!=NULL);
+			token = strtok(line, " ");
+			i=0;
+			while(token!=NULL)
+			{
+				arg[i]=token;
+				token=strtok(NULL, " ");
+				i++;		 // after while loop, i equals the number of components in arg.
+			}            		// arg[0] = command. (e.g., "cd" )
+			int j=i-1;
+			arg[j][strlen(arg[j])-1]='\0';
+			arg[i]=NULL;
+			if(strcmp(arg[0],"cd")==0)
+			{
+				if(i==1)
+				{
+					print_err();  
+				}
+				if(i>2)
+				{
+					print_err();
+				}
+				else if(i==2)
+				{
+					if(strcmp(arg[1],"~")==0)
+					{
+						if(chdir(getenv("HOME"))==-1)
+						{
+							print_err();
+						}
+					}
+					else
+					{
+						if(chdir(arg[1])==-1)
+						{
+							print_err(); 
+						}
+					}
+				}
+				continue;
+			}
+			if(strcmp(arg[0],"path")==0)
+			{
+				if(i==1) //command 'path' is passed with 0 argument.
+				{
+					setenv("PATH","",1);
+				}
+				else if(i>1)
+				{
+					char new_path[255];
+					memset(new_path,0,sizeof(new_path));
+					for(int y=0;y<i-1;y++)
+					{
+						if(y>0)
+							strcat(new_path,":");
+						strcat(new_path,arg[y+1]);
+					}
+					setenv("PATH",new_path,1);
+				}
+				//printf("revised path: %s\n",getenv("PATH"));
+				continue;
+			}
+			if(strcmp(arg[0],"exit")==0)
+			{
+				if(i>1) // one or more arguments passed to exit, error
+				{
+					print_err();
+					exit(0);
+				}
+				exit(0);	
+			}
+
 
 			pid_t pid = fork();
 			if(pid<0)
 			{
 				print_err(); // fork fail
+				printf("fork fail\n");
 			}
 			if(pid==0)
 			{
-				char* token;
-				char* arg[10];
-				assert(line!=NULL);
-				token = strtok(line, " ");
-				i=0;
-				while(token!=NULL)
+				//printf("child: my pid is... %d\n",getpid());
+				if(strstr(arg[0],".sh")!=NULL) // if arg[0] contains ".sh"
 				{
-					arg[i]=token;
-					token=strtok(NULL, " ");
-					i++;		 // after while loop, i equals the number of components in arg.
-				}            		// arg[0] = command. (e.g., "cd" )
-				int j=i-1;
-				arg[j][strlen(arg[j])-1]='\0';
-				arg[i]=NULL;
-				if(strcmp(arg[0],"cd")==0)
-				{
-					if(i==1)
-					{
-						print_err();  
-					}
-					if(i>2)
-					{
-						print_err();
-					}
-					else if(i==2)
-					{
-						if(strcmp(arg[1],"~")==0)
-						{
-							if(chdir(getenv("HOME"))==-1)
-							{
-								print_err();
-							}
-						}
-						else
-						{
-							if(chdir(arg[1])==-1)
-							{
-								print_err(); 
-							}
-						}
-					}
-				}
-				
-				if(strcmp(arg[0],"path")==0)
-				{
-					if(i==1) //command 'path' is passed with 0 argument.
-					{
-						setenv("PATH","",1);
-					}
-					else if(i>1)
-					{
-						char new_path[255];
-						new_path[0]='\0';
-						for(int y=0;y<i-1;y++)
-						{
-							if(y>0)
-								strcat(new_path,":");
-							strcat(new_path,argv[y+1]);
-						}
-						setenv("PATH",new_path,1);
-					}
-				}
-				if(strcmp(arg[0],"exit")==0)
-				{
-					if(i>1) // one or more arguments passed to exit, error
-					{
-						print_err();
-					}
-
-					flag=0;
-					close(fd[0]);
-					write(fd[1],&flag,sizeof(flag));
-					close(fd[1]);
-					exit(0);
-				}
-
-							
-				if(strcmp(arg[0],"cd")!=0 && strcmp(arg[0],"exit")!=0 && strcmp(arg[0],"path")!=0)
-				{
-					sprintf(path,"/bin/%s",arg[0]);
+					strcpy(path,getenv("PATH"));
+					strcat(path,"/");
+					strcat(path,arg[0]);
+					/*
 					if(access(path,X_OK)==-1)
 					{
-						sprintf(path,"/usr/bin/%s",arg[0]);
 						if(access(path,X_OK)==-1)
 						{
 							print_err(); // binary access fail
-							//printf("%d\n",2);
+							_exit(status);
 						}
 					}
+					*/
 					execv(path,arg);
-					printf("This shouldn't be printed\n");
 				}
+				sprintf(path,"/bin/%s",arg[0]);
+				if(access(path,X_OK)==-1)
+				{
+					sprintf(path,"/usr/bin/%s",arg[0]);
+					if(access(path,X_OK)==-1)
+					{
+						print_err(); // binary access fail
+						_exit(status);
+					}
+				}
+				else
+					execv(path,arg);
+				//printf("execv... \n");
+					//printf("execv error\n");
+				printf("This shouldn't be printed\n");
 			}
 			if(pid>0)
 			{
 				wait(&status);
-				close(fd[1]);
-				read(fd[0],&flag,sizeof(flag));
-				close(fd[0]);
-				if(flag==0)	exit(0);
-				//free(line);
+				//printf("parent: I'm parent of... %d\n",pid);
 			}
 
 		}
 		free(line);
-		
-
 	} // end of if(argc>1)
 	
 	else if(argc==1)
@@ -179,8 +186,6 @@ int main(int argc, char** argv)
 			int status; // for wait
 			size_t linecap;
 			char path[30];
-
-
 
 			printf("wish> ");
 			getline(&line,&linecap,stdin);
